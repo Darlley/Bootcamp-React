26b255fca232e820c5b904c9b3492f60286f9df0
Darlley <systembrtt@gmail.com>
2021-08-18 11:32:30 -0400

    Dia 1 ok

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
-import a from './main2';
+import './style.css'
 
-console.log('a: ', a)
+const app = document.querySelector('[data-js="app"]')
+const link = document.querySelector('[data-js="link"]')
+const button = document.querySelector('[data-js="button"]')
+
+link.addEventListener('click', (event) => {
+  event.preventDefault()
+  console.log('clicou', event.target)
+  alert('link')
+}, false)
+
+button.addEventListener('click', (event) => {
+  event.preventDefault()
+  event.stopPropagation()
+  alert('button')
+})
