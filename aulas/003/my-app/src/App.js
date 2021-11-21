3f538cb848973d259de32e6cdd3de9c9db566236
Darlley <systembrtt@gmail.com>
2021-11-21 19:16:12 -0400

    att

--- aulas/003/my-app/src/App.js
+++ aulas/003/my-app/src/App.js
@@ -1,28 +1,37 @@
-import React, {useState, InputHTMLAttributes} from 'react'
+import React, {useEffect, useState} from 'react';
+
+function Root(){
+  return <App />
+}
 
 function App(){
+  const [counter, setCounter] = useState(0)
+  console.log('1) Inicio da montagem do componente')
 
-  const [inputValue, setInputValue] = useState('')
-  console.log(`inputValue (estado): ${inputValue}`)
+  useEffect(() => {
+    document.title = 'New title'
+    console.log('5) seção - efeitos colaterais')
 
-  function handleChange(event){
-    const targetInputValue = Number(event.target.value)
-    // console.log(`inputValue (component): ${targetInputValue}`)
-    console.log(typeof targetInputValue)
-    if(!isNaN(targetInputValue)){
-      setInputValue(targetInputValue)
+    const id = setInterval(() => {
+      //setCounter(c => c + 1)
+      console.log('3) efeito colateral')
+    }, 1000)
+
+    return () => {
+      clearInterval(id)
+      console.log('4) limpando efeitos colaterais')
     }
-  }
-  function handleClick(){
-    alert(inputValue)
-  }
+  })
 
+  console.log('2) Preparando para montar o componente')
   return (
-    <form>
-      <input value={inputValue} onChange={handleChange} />
-      <button type="" onClick={handleClick}>Enviar</button>
-    </form>
+    <React.Fragment>
+      <h1>App</h1>
+      <h3>{counter}</h3>
+      <button onClick={() => counter<10 && setCounter(c => c+1)}>+</button>
+      <button onClick={() => counter>0 && setCounter(c => c-1)}>-</button>
+    </React.Fragment>
   )
 }
 
-export default App
+export default Root
--- aulas/003/my-app/src/index.js
+++ aulas/003/my-app/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Root from './App';
 
 ReactDOM.render(
-  <App />,
+  <Root />,
   document.querySelector('#root')
 )
