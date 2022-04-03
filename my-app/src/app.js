e84a786abf0469972c77f8f347deccc4442021a2
Darlley <systembrtt@gmail.com>
2022-04-03 17:55:40 -0400

    aula 09

--- my-app/src/app.js
+++ my-app/src/app.js
@@ -1,26 +1,12 @@
-// const lista = [1,2,3]
-const lista = [
-  {
-    name: "Home",
-    link: "/"
-  },
-  {
-    name: "Sobre",
-    link: "/sobre"
-  }
-]
 
-function App(){
+const App = () => {
+
+  function handleClick(){
+    alert('Clicou')
+  }
+
   return (
-    <ul>
-      {lista.map((item, index) => {
-        return (
-          <li key={index}>
-            <a href={item.link}>{item.name}</a>
-          </li>
-        )
-      })}
-    </ul>
+    <button onClick={handleClick}>Clique aqui</button>
   )
 }
 
